9a1a7720d31d913efb7d3e600a1ac7b1627747f5
Nicholas Tan Yu Zhe <nicholas.290696@gmail.com>
2016-12-30 02:37:37 +0800

    Add files via upload
    Changed the way to rename column name and add more outliers checker

--- Data Mining Part 1.R
+++ Data Mining Part 1.R
@@ -1,16 +1,41 @@
-setwd("C:/Users/NicholasTan/Desktop")
 data <- read.csv('xAPI-Edu-Data.csv')
 data
 dim(data)
 head(data)
 
-is.na(data) #check for na
+#check for na
+any(is.na(data))
 
-length(unique(data)) == nrow(data) #check for duplicates
+#check for duplicates
+sum(duplicated(data))
+data <- data[!duplicated(data),]
 
-colnames(data) <- c("gender", "nationality", "place of birth", "education level", "grade", "classroom", "course", "semester", "parent responsible for student", "raised hand", "visited resources", "viewed announcement", "participate in discussion group", "parent answering survey", "parent satisfication with school", "student absence days", "student performance level")
+#Fixed the inconsistency of column names and correct some column names to match the value
+names(data)[names(data) == 'NationalITy'] <- 'nationality'
+names(data)[names(data) == 'PlaceofBirth'] <- 'place_of_birth'
+names(data)[names(data) == 'StageID'] <- 'education_level'
+names(data)[names(data) == 'GradeID'] <- 'grade'
+names(data)[names(data) == 'SectionID'] <- 'classroom'
+names(data)[names(data) == 'Topic'] <- 'course'
+names(data)[names(data) == 'Semester'] <- 'semester'
+names(data)[names(data) == 'Relation'] <- 'parent_responsible_for_student'
+names(data)[names(data) == 'raisedhands'] <- 'raised_hands'
+names(data)[names(data) == 'VisITedResources'] <- 'visited_resources'
+names(data)[names(data) == 'AnnouncementsView'] <- 'viewed_announcement'
+names(data)[names(data) == 'Discussion'] <- 'participate_in_discussion_group'
+names(data)[names(data) == 'ParentAnsweringSurvey'] <- 'parent_answering_survey'
+names(data)[names(data) == 'ParentschoolSatisfaction'] <- 'parent_satisfication_with_school'
+names(data)[names(data) == 'StudentAbsenceDays'] <- 'student_absence_days'
+names(data)[names(data) == 'Class'] <- 'student_performance_level'
 data
 
-outliers <- boxplot(data$"raised hand", plot=FALSE)$out #check for outliers
-outliers
+#Check for outliers
+outliersRaisedHands <- boxplot(data$raised_hands, plot=FALSE)$out
+outliersVisitedResources <- boxplot(data$visited_resources, plot=FALSE)$out
+outliersViewedAnnouncement <- boxplot(data$viewed_announcement, plot=FALSE)$out
+outliersParticipateInDiscussionGroup <- boxplot(data$participate_in_discussion_group, plot=FALSE)$out
+outliersRaisedHands
+outliersVisitedResources
+outliersViewedAnnouncement
+outliersParticipateInDiscussionGroup
 
